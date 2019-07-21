f974e65f96518fa4f3b5dbf08fc73c7e5d97f51e
kanbo123 <52125963+kanbo123@users.noreply.github.com>
2019-07-21 10:18:58 +0800

    Add files via upload

---
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -0,0 +1,76 @@
+package com.aliware.tianchi;
+
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.RpcException;
+import org.apache.dubbo.rpc.cluster.LoadBalance;
+
+import com.aliware.tianchi.comm.ServerLoadInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author daofeng.xjf
+ *
+ * 负载均衡扩展接口
+ * 必选接口，核心接口
+ * 此类可以修改实现，不可以移动类或者修改包名
+ * 选手需要基于此类实现自己的负载均衡算法
+ */
+public class UserLoadBalance implements LoadBalance{
+    
+    
+    @Override
+    public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
+        
+        int size = invokers.size();
+        // 总权重
+        int totalWeight = 0;
+        List<Integer> hasPermitArr = new ArrayList<>();
+        List<Integer> weightArr = new ArrayList<>();
+        // 首先获取invoker对应的服务端耗时最大的索引
+        for(int index=0;index<size;index++){
+            Invoker<T> invoker = invokers.get(index);
+            ServerLoadInfo serverLoadInfo = UserLoadBalanceService.getServerLoadInfo(invokers.get(index));
+            AtomicInteger limiter = UserLoadBalanceService.getAtomicInteger(invoker);
+            if(serverLoadInfo != null){
+                int permits = limiter.get();
+                int weight = serverLoadInfo.getWeight();
+                if(permits > 0){
+                    int avgSpentTime = serverLoadInfo.getAvgSpendTime();
+                    if(avgSpentTime == 0){
+                        continue;
+                    }
+                    // CPU数量*(1秒内处理个数)
+                    int realWeight = weight * (1000/avgSpentTime);
+                    hasPermitArr.add(index);
+                    weightArr.add(realWeight);
+                    totalWeight = totalWeight+realWeight;
+                }
+            }
+        }
+        
+        
+        if(hasPermitArr.size() == 0){
+            return invokers.get(ThreadLocalRandom.current().nextInt(invokers.size()));
+        }
+        // 根据服务端配置和平均耗时计算权重
+        int offsetWeight = ThreadLocalRandom.current().nextInt(totalWeight);
+        
+        for(int i=0;i<hasPermitArr.size();i++){
+            int index = hasPermitArr.get(i);
+            int currentWeight = weightArr.get(i);
+            offsetWeight  = offsetWeight - currentWeight;
+            if (offsetWeight < 0) {
+                return invokers.get(index);
+            }
+        }
+        
+        return invokers.get(ThreadLocalRandom.current().nextInt(invokers.size()));
+    }
+    
+}
